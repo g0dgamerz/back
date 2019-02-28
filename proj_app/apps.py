2468c66731e4a7f7ea491a4963e275465908445d
Sumedh Shakya <sumedhshakya11@gmail.com>
2019-02-28 22:32:54 +0545

    added project app wiht .gitignore file and requiremets.txt file

---
+++ proj_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProjAppConfig(AppConfig):
+    name = 'proj_app'
--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    
+    'rest_auth',
+    'rest_auth.registration',
+    'proj_app',
+
     'corsheaders',
 
 ]
